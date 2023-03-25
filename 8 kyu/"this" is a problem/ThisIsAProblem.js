4e624c44203c749b8ff4cf09d9f1cf61e1b0eade
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-03-25 13:01:01 +0100

    Create ThisIsAProblem.js

---
+++ 8 kyu/"this" is a problem/ThisIsAProblem.js
@@ -0,0 +1,6 @@
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = first+" "+last;
+  return {firstName: first, lastName: last, name: (this.firstName + ' ' + this.lastName)};
+}
